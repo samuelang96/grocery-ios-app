6e95892056c79e023f671564cee6995ab90f3db9
samuelang96 <samuel.ang.prog@gmail.com>
2018-02-12 18:29:40 -0600

    Added shopping list object

---
+++ AVCamBarcodeUsingAVFoundationtoDetectBarcodesandFaces/AVCamBarcode/ShoppingList.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingList.swift
+//  AVCamBarcode
+//
+//  Created by Samuel Ang on 2/12/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingList: NSObject {
+    var name: String
+    init(newName: String){
+        name = newName;
+    }
+}
