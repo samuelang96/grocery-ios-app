6fa37a4add1b8671e43fa30e344b277fb04a964e
samuelang96 <samuel.ang.prog@gmail.com>
2018-02-20 09:59:21 -0600

    Fixed barcode reference error

--- AVCamBarcodeUsingAVFoundationtoDetectBarcodesandFaces/AVCamBarcode/DemoViewController.swift
+++ AVCamBarcodeUsingAVFoundationtoDetectBarcodesandFaces/AVCamBarcode/DemoViewController.swift
@@ -11,8 +11,12 @@ import Alamofire
 class DemoViewController: UIViewController {
 
     @IBOutlet weak var demoTextView: UITextView!
+    @IBOutlet weak var barcodeTextField: UITextField!
     @IBAction func getSampleProduct(_ sender: Any) {
-        testRequest();
+        //testRequest();
+        //0044600046907
+        //0070847012474
+        testBarcodeRequest(barcode: self.barcodeTextField.text!);
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +39,18 @@ class DemoViewController: UIViewController {
         //let request = Alamofire.request("http://httpbin.org/get", method: .get)
         debugPrint(request)
     }
+    
+    func testBarcodeRequest(barcode: String){
+            //Alamofire.request("http://heb.cs.trinity.edu/api/products", method: .get).responseJSON { response in
+            Alamofire.request("http://heb.cs.trinity.edu/api/products/" + barcode, method: .get).responseJSON{response in
+                print(response)
+                let rData = response.description
+                self.demoTextView.text = rData
+            }
+            //let request = Alamofire.request("http://httpbin.org/get", method: .get)
+            debugPrint(request)
+        }
+    
 
     /*
     // MARK: - Navigation
